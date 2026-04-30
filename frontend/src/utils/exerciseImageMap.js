d5a0b3e8245fa8adce236a50046179c7c8c00c5c
Liam Bruno <liambruno007@gmail.com>
2026-04-30 10:52:56 +0200

    feat(frontend): add curated local exercise images and mapping

---
+++ frontend/src/utils/exerciseImageMap.js
@@ -0,0 +1,35 @@
+import benchPress from '../assets/exercises/bench-press.png'
+import inclineDumbbellPress from '../assets/exercises/incline-dumbbell-press.png'
+import latPulldown from '../assets/exercises/lat-pulldown.png'
+import barbellRow from '../assets/exercises/barbell-row.png'
+import squat from '../assets/exercises/squat.png'
+import romanianDeadlift from '../assets/exercises/romanian-deadlift.png'
+import standingCalfRaise from '../assets/exercises/standing-calf-raise.png'
+import shoulderPress from '../assets/exercises/shoulder-press.png'
+import lateralRaise from '../assets/exercises/lateral-raise.png'
+import barbellCurl from '../assets/exercises/barbell-curl.png'
+import tricepsPushdown from '../assets/exercises/triceps-pushdown.png'
+import crunch from '../assets/exercises/crunch.png'
+import cycling from '../assets/exercises/cycling.png'
+import abCrunchMachine from '../assets/exercises/ab-crunch-machine.png'
+
+const exerciseImageMap = {
+    'Bench Press': benchPress,
+    'Incline Dumbbell Press': inclineDumbbellPress,
+    'Lat Pulldown': latPulldown,
+    'Barbell Row': barbellRow,
+    'Squat': squat,
+    'Romanian Deadlift': romanianDeadlift,
+    'Standing Calf Raise': standingCalfRaise,
+    'Shoulder Press': shoulderPress,
+    'Lateral Raise': lateralRaise,
+    'Barbell Curl': barbellCurl,
+    'Triceps Pushdown': tricepsPushdown,
+    'Crunch': crunch,
+    'Bauchpresse': abCrunchMachine,
+    'Cycling': cycling,
+}
+
+export function getExerciseImage(exerciseName) {
+    return exerciseImageMap[exerciseName] || null
+}
